a452c5a05881940645126bca9aefae156f1d9cc8
unknown <ggpeaol@gmail.com>
2023-05-02 20:53:54 -0300

    adicionando arquivos assembly para teste

---
+++ TrabalhoSO/Mars4_5/ArquivosParaTeste/macro.asm
@@ -0,0 +1,16 @@
+.macro SyscallFork(%endereco, %prioridade)
+  li $v0, 60
+  la $a0, %endereco
+  la $a1, %prioridade
+  syscall
+.end_macro 
+
+.macro SyscallProcessChange
+  li $v0, 61
+  syscall
+.end_macro
+
+ .macro SyscallProcessTerminate
+  li $v0, 62
+  syscall
+.end_macro  
